c51aab34d9cf0b9a22a12d450ee11fce34c5e5ea
jdank417 <dankj@wit.edu>
2024-10-01 22:16:29 -0400

    Formating Improvements. No technical changes

--- remote_monitor_json.ps1
+++ remote_monitor_json.ps1
@@ -18,22 +18,17 @@ $systemInfo = @{
     Disk = ""
 }
 
-
 # Get CPU information, filter out unwanted messages
 $cpuInfo = plink -ssh "$user@$ipAddress" -pw "$password" "top -b -n1 | grep 'Cpu(s)'" | Select-String -NotMatch "Access granted. Press Return to begin session."
 $systemInfo.CPU = $cpuInfo -join "`n"
-
-
 
 # Get memory information, filter out unwanted messages
 $memoryInfo = plink -ssh "$user@$ipAddress" -pw "$password" "free -m" | Select-String -NotMatch "Access granted. Press Return to begin session."
 $systemInfo.Memory = $memoryInfo -join "`n"
 
-
 # Get disk information, filter out unwanted messages
 $diskInfo = plink -ssh "$user@$ipAddress" -pw "$password" "df -h" | Select-String -NotMatch "Access granted. Press Return to begin session."
 $systemInfo.Disk = $diskInfo -join "`n"
-
 
 # Convert the hashtable to JSON and write it to the file
 $systemInfo | ConvertTo-Json -Depth 3 | Out-File -FilePath $resultsFile
